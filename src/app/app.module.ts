5ad8380aad3e8b9371983e13274838e62e913fe7
Jon Nakkerud <jonnakkerud@Jons-MacBook-Air.local>
2020-04-10 10:11:20 -0700

    nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line: component-selector
+    selector: 'nav-bar',
+    templateUrl: 'nav-bar.component.html',
+    styleUrls: ['./nav-bar.component.scss'],
+})
+
+export class NavBarComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
